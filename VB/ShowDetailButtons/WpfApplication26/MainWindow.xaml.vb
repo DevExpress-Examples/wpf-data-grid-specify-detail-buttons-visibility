23561db2891912f3b8d85deced116d8d27983ab7
DevExpressExampleBot <fakeEmail>
2022-12-21 19:23:00 +0400

    Source auto update [skip ci]

--- VB/ShowDetailButtons/WpfApplication26/MainWindow.xaml.vb
+++ VB/ShowDetailButtons/WpfApplication26/MainWindow.xaml.vb
@@ -1,47 +1,37 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
 Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 Imports System.Globalization
 
 Namespace WpfApplication26
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			grid1.ItemsSource = New DataSource().Categories
 
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
 
-	<ValueConversion(GetType(Object), GetType(Boolean))> _
-	Public Class MyConverter
-		Implements IValueConverter
-		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
-			' Obtaining the value to be converted 
-			Dim categoryValue As String = CStr(value)
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid1.ItemsSource = New DataSource().Categories
+        End Sub
+    End Class
 
-			' Specifying values for which to show expand buttons
-			Dim categories() As String = { "First", "Third" }
-			If categories.Contains(categoryValue) Then
-				Return True
-			End If
+    <ValueConversion(GetType(Object), GetType(Boolean))>
+    Public Class MyConverter
+        Implements IValueConverter
 
-			' Disable expand button if the value isn't in the list
-			Return False
-		End Function
-		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
-			Return Nothing
-		End Function
-	End Class
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            ' Obtaining the value to be converted 
+            Dim categoryValue As String = CStr(value)
+            ' Specifying values for which to show expand buttons
+            Dim categories As String() = New String() {"First", "Third"}
+            If categories.Contains(categoryValue) Then Return True
+            ' Disable expand button if the value isn't in the list
+            Return False
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return Nothing
+        End Function
+    End Class
 End Namespace
--- VB/WpfApplication26/Data.vb
+++ VB/WpfApplication26/Data.vb
@@ -1,80 +1,96 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Collections.Generic
 
 Namespace WpfApplication26
 
-	Public Class Category
-		Private name_Renamed As String
-		Private products_Renamed As List(Of Product)
-		Public Sub New(ByVal name As String)
-			Me.name_Renamed = name
-			products_Renamed = New List(Of Product)()
-		End Sub
-		Public Property Name() As String
-			Get
-				Return name_Renamed
-			End Get
-			Set(ByVal value As String)
-				name_Renamed = value
-			End Set
-		End Property
-		Public ReadOnly Property Products() As List(Of Product)
-			Get
-				Return products_Renamed
-			End Get
-		End Property
-	End Class
-	Public Class Product
-		Private name_Renamed As String
-		Private price_Renamed As Double
-		Public Sub New(ByVal name As String, ByVal price As Double)
-			Me.name_Renamed = name
-			Me.price_Renamed = price
-		End Sub
-		Public Property Name() As String
-			Get
-				Return name_Renamed
-			End Get
-			Set(ByVal value As String)
-				name_Renamed = value
-			End Set
-		End Property
-		Public Property Price() As Double
-			Get
-				Return price_Renamed
-			End Get
-			Set(ByVal value As Double)
-				price_Renamed = value
-			End Set
-		End Property
-	End Class
+    Public Class Category
 
-	Public Class DataSource
-		Private categories_Renamed As List(Of Category)
-		Public Sub New()
-			categories_Renamed = New List(Of Category)()
-			categories_Renamed.Add(New Category("First"))
-			categories_Renamed(0).Products.Add(New Product("Product 1.1", 10))
-			categories_Renamed(0).Products.Add(New Product("Product 1.2", 20))
-			categories_Renamed(0).Products.Add(New Product("Product 1.3", 30))
-			categories_Renamed(0).Products.Add(New Product("Product 1.4", 40))
-			categories_Renamed.Add(New Category("Second"))
-			categories_Renamed(1).Products.Add(New Product("Product 2.1", 10))
-			categories_Renamed(1).Products.Add(New Product("Product 2.2", 20))
-			categories_Renamed.Add(New Category("Third"))
-			categories_Renamed(2).Products.Add(New Product("Product 3.1", 10))
-			categories_Renamed(2).Products.Add(New Product("Product 3.2", 20))
-			categories_Renamed(2).Products.Add(New Product("Product 3.4", 40))
-			categories_Renamed.Add(New Category("Fourth"))
-			categories_Renamed(3).Products.Add(New Product("Product 4.1", 10))
-			categories_Renamed(3).Products.Add(New Product("Product 4.2", 20))
-			categories_Renamed(3).Products.Add(New Product("Product 4.3", 30))
-			categories_Renamed(3).Products.Add(New Product("Product 4.4", 40))
-		End Sub
-		Public ReadOnly Property Categories() As List(Of Category)
-			Get
-				Return categories_Renamed
-			End Get
-		End Property
-	End Class
+        Private nameField As String
+
+        Private productsField As List(Of Product)
+
+        Public Sub New(ByVal name As String)
+            nameField = name
+            productsField = New List(Of Product)()
+        End Sub
+
+        Public Property Name As String
+            Get
+                Return nameField
+            End Get
+
+            Set(ByVal value As String)
+                nameField = value
+            End Set
+        End Property
+
+        Public ReadOnly Property Products As List(Of Product)
+            Get
+                Return productsField
+            End Get
+        End Property
+    End Class
+
+    Public Class Product
+
+        Private nameField As String
+
+        Private priceField As Double
+
+        Public Sub New(ByVal name As String, ByVal price As Double)
+            nameField = name
+            priceField = price
+        End Sub
+
+        Public Property Name As String
+            Get
+                Return nameField
+            End Get
+
+            Set(ByVal value As String)
+                nameField = value
+            End Set
+        End Property
+
+        Public Property Price As Double
+            Get
+                Return priceField
+            End Get
+
+            Set(ByVal value As Double)
+                priceField = value
+            End Set
+        End Property
+    End Class
+
+    Public Class DataSource
+
+        Private categoriesField As List(Of Category)
+
+        Public Sub New()
+            categoriesField = New List(Of Category)()
+            categoriesField.Add(New Category("First"))
+            categoriesField(0).Products.Add(New Product("Product 1.1", 10))
+            categoriesField(0).Products.Add(New Product("Product 1.2", 20))
+            categoriesField(0).Products.Add(New Product("Product 1.3", 30))
+            categoriesField(0).Products.Add(New Product("Product 1.4", 40))
+            categoriesField.Add(New Category("Second"))
+            categoriesField(1).Products.Add(New Product("Product 2.1", 10))
+            categoriesField(1).Products.Add(New Product("Product 2.2", 20))
+            categoriesField.Add(New Category("Third"))
+            categoriesField(2).Products.Add(New Product("Product 3.1", 10))
+            categoriesField(2).Products.Add(New Product("Product 3.2", 20))
+            categoriesField(2).Products.Add(New Product("Product 3.4", 40))
+            categoriesField.Add(New Category("Fourth"))
+            categoriesField(3).Products.Add(New Product("Product 4.1", 10))
+            categoriesField(3).Products.Add(New Product("Product 4.2", 20))
+            categoriesField(3).Products.Add(New Product("Product 4.3", 30))
+            categoriesField(3).Products.Add(New Product("Product 4.4", 40))
+        End Sub
+
+        Public ReadOnly Property Categories As List(Of Category)
+            Get
+                Return categoriesField
+            End Get
+        End Property
+    End Class
 End Namespace
